6eb2436e14055b5f26e82d294f76e3bbcb298a10
Zach <zevans2@missouriwestern.edu>
2018-06-29 22:55:17 -0500

    Initial commit

---
+++ KFSelector.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="row">
+  <h2>KeepFlo Selector</h2>
+  <b-button :size="lg" :variant="primary" onclick="getDistributors()">Crick Here</b-button>
+</div>
+</template>
+
+<script>
+  import { getDistributors } from "../scripts/handler";
+
+  let distributors = getDistributors();
+  export default {
+        name: "k-f-selector",
+        distributors: distributors
+    }
+</script>
+
+<style scoped>
+
+</style>
